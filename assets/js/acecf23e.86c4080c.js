0c08fe81be4916d3455da6539665f0afb20cbc56
CagriCatik <CagriCatik@users.noreply.github.com>
2024-12-29 22:01:46 +0000

    deploy: 81aa00481574501c2dfd597b979e3b932f6c36eb

---
+++ assets/js/acecf23e.86c4080c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_document=self.webpackChunkmy_document||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/SOVD/blog","blogTitle":"Blog","authorsListPath":"/SOVD/blog/authors"}')}}]);
---
+++ assets/js/d83fc43d.cfe702d8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_document=self.webpackChunkmy_document||[]).push([[9368],{3207:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/SOVD/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
